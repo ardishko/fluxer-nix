df727cd129000a4d6afd656e6e4c7398c99aad5f
ardishco <ardishco@protonmail.com>
2026-02-28 19:27:48 +0200

    Created repo. Welcome, Discord haters

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Custom Fluxer Nix package";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+        {
+          fluxer = pkgs.callPackage ./pkgs/fluxer { };
+        }
+      );
+    };
+}
